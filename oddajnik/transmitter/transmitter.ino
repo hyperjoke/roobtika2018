b36effe70e5d0965e2fa2b1532df6b902d8cde04
MartiP33 <martin.pusar@gmail.com>
2018-06-28 16:17:59 +0200

    updatean transmitter ki naj bi zares delu in je koncan

--- oddajnik/transmitter/transmitter.ino
+++ oddajnik/transmitter/transmitter.ino
@@ -1,19 +1,18 @@
 #include <SPI.h>
 #include "RF24.h"
-
+#define N_CH 4
 
 RF24 Radio(9, 10);          //CE, CSN
 byte addresses = "12345";
 bool debug = false;
-
 struct transmit_data {
   unsigned short int ch[4];
-
 };
-
-int trim_var_x = 0;
-int trim_var_y = 0;
+/*int trim_var_x = 0;
+  int trim_var_y = 0;*/
 transmit_data cont_data;
+int a;
+
 
 void setup() {
   Serial.begin (115200);
@@ -21,97 +20,67 @@ void setup() {
   Radio.setPALevel(RF24_PA_LOW);
   Radio.openWritingPipe (addresses);
 
-  pinMode (5, INPUT_PULLUP);
-  pinMode (16, INPUT_PULLUP);
-
-  cont_data.ch[0] = 512;
-  cont_data.ch[1] = 512;
-  cont_data.ch[2] = 512;
-  cont_data.ch[3] = 512;
+  for ( a = 0; a < N_CH; a++) {
+    pinMode(14 + a, INPUT);
+    cont_data.ch[a] = 512;
+  }
 }
-
-
-
 
 void loop() {
   joystickData ();
-  trimCode();
+  /*trimCode();*/
 
   if (debug) {
     debugFunction();
   } else {
     sendData ();
   }
-
 }
+
 
 void sendData() {
   Radio.write ( &cont_data, sizeof(transmit_data) );
 }
 
-void joystickData () {    //Za zrihtat joystick....
-  boolean last_button;
-  unsigned long timer;
-  int y = 0;
-  int THR[4] = {0, 341, 682, 1023};
-  bool swVal;
-  cont_data.ch[0] = analogRead(0) + trim_var_x;
-  cont_data.ch[1] = analogRead(1) + trim_var_y;
-  cont_data.ch[2] = THR[y];
 
-  if (millis() - timer > 25) {
-    swVal = digitalRead(16);
-    if (last_button == 0 && swVal == 0 && y < 3) {
-      y++;
-      last_button = !last_button;
-    }
-    if (last_button == 1 && swVal == 0) {
-      last_button = !last_button;
-    }
-    if (last_button == 0 && swVal == 0 && y == 3) {
-      y = 0;
-      last_button = !last_button;
-    }
-    timer = millis();
+void joystickData () {
+  for (a = 0; a < N_CH; a++) {
+    cont_data.ch[a] = analogRead(a);
   }
-
-  /*
-    if (cont_data.ch[0] < 0) cont_data.ch[0] = 0;
-    if (cont_data.ch[0] > 1023) cont_data.ch[0] = 1023;
-    if (cont_data.ch[1] < 0) cont_data.ch[1] = 0;
-    if (cont_data.ch[1] > 1023) cont_data.ch[1] = 1023;
-  */
 }
+/*
+  if (cont_data.ch[0] < 0) cont_data.ch[0] = 0;
+  if (cont_data.ch[0] > 1023) cont_data.ch[0] = 1023;
+  if (cont_data.ch[1] < 0) cont_data.ch[1] = 0;
+  if (cont_data.ch[1] > 1023) cont_data.ch[1] = 1023;
+*/
 
 
 
-void trimCode () {        //koda za kalibriranje
+/*void trimCode () {        //koda za kalibriranje
   bool button;
-  bool last_button;
   button = digitalRead (5);
   button = !button;
 
-  if (cont_data.ch[0] > 650 && button == HIGH && last_button == false /*&& cont_data.ch[0] < 1013*/) {
+  if (cont_data.ch[0] > 650 && button == HIGH && last_button == !button && cont_data.ch[0] < 1013) {
     trim_var_x += 10;
   }
-  if (cont_data.ch[0] < 200 && button == HIGH && last_button == false /*&& cont_data.ch[0] > 10*/) {
+  if (cont_data.ch[0] < 200 && button == HIGH && last_button == !button && cont_data.ch[0] > 10) {
     trim_var_x -= 10;
   }
-
-  if (cont_data.ch[1] > 650 && button == HIGH && last_button == false /*&& cont_data.ch [1] < 1013*/) {
+  if (cont_data.ch[1] > 650 && button == HIGH && last_button == !button && cont_data.ch [1] < 1013) {
     trim_var_y += 10;
   }
-  if (cont_data.ch[1] < 200 && button == HIGH && last_button == false /*&& cont_data.ch[1] > 10*/) {
+  if (cont_data.ch[1] < 200 && button == HIGH && last_button == !button /*&& cont_data.ch[1] > 10) {
     trim_var_y -= 10;
   }
-
   last_button = button;
-}
+  }*/
 
 
 void debugFunction() {
-  for (int i = 0; i < 4; i++) {
-    Serial.print(cont_data.ch[i]);
-    Serial.print(",");
+  for (a = 0; a < N_CH; a++) {
+    Serial.print(cont_data.ch[a]);
+    Serial.println ();
   }
 }
